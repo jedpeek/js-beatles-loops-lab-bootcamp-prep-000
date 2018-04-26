44b6cfe2c534def930b914bf29bba84f52035b5b
Jed Peek <jedpeek@yahoo.com>
2018-04-26 20:32:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts(array){
   var arr = [];
   let i = array.length
   while(i < 0){
-    arr.shift(array[i] + "!!!");
+    arr.unshift(array[i] + "!!!");
     --i;
   }
   return arr;
